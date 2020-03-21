8b9dd1d0bd47593ee278f48e8b5b5287f05d331d
AdrianRubioT <35344029+AdrianRubioT@users.noreply.github.com>
2020-03-21 17:25:05 +0100

    cambiado jscrollpane por paneles normales y cambiado los nombres de las listas de las targetas

--- src/vista/Interfaz.java
+++ src/vista/Interfaz.java
@@ -4,17 +4,16 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
-import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import javax.swing.JPanel;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JButton;
-import javax.swing.JScrollBar;
-import javax.swing.JScrollPane;
 import java.awt.GridLayout;
 
 public class Interfaz {
@@ -103,54 +102,11 @@ public class Interfaz {
 		gbc_areaTrabajo.gridy = 1;
 		panHorario.add(areaTrabajo, gbc_areaTrabajo);
 		
-		JPanel panEmpleados = new JPanel();
-		GridBagConstraints gbc_panEmpleados = new GridBagConstraints();
-		gbc_panEmpleados.insets = new Insets(0, 0, 0, 5);
-		gbc_panEmpleados.fill = GridBagConstraints.VERTICAL;
-		gbc_panEmpleados.gridx = 1;
-		gbc_panEmpleados.gridy = 0;
-		frame.getContentPane().add(panEmpleados, gbc_panEmpleados);
-		GridBagLayout gbl_panEmpleados = new GridBagLayout();
-		gbl_panEmpleados.columnWidths = new int[]{91, 0};
-		gbl_panEmpleados.rowHeights = new int[]{14, 27, 0, 0};
-		gbl_panEmpleados.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-		gbl_panEmpleados.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
-		panEmpleados.setLayout(gbl_panEmpleados);
-		
-		JLabel tituloModulos = new JLabel("Lista de modulos");
-		GridBagConstraints gbc_tituloModulos = new GridBagConstraints();
-		gbc_tituloModulos.insets = new Insets(0, 0, 5, 0);
-		gbc_tituloModulos.anchor = GridBagConstraints.NORTH;
-		gbc_tituloModulos.gridx = 0;
-		gbc_tituloModulos.gridy = 0;
-		panEmpleados.add(tituloModulos, gbc_tituloModulos);
-		
-		JPanel panel_1 = new JPanel();
-		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_1.fill = GridBagConstraints.VERTICAL;
-		gbc_panel_1.gridx = 0;
-		gbc_panel_1.gridy = 1;
-		panEmpleados.add(panel_1, gbc_panel_1);
-		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
-		
-		JButton button = new JButton("A\u00F1adir");
-		panel_1.add(button);
-		
-		JButton button_1 = new JButton("Eliminar");
-		panel_1.add(button_1);
-		
-		JScrollPane listaEmpleados = new JScrollPane();
-		GridBagConstraints gbc_listaEmpleados = new GridBagConstraints();
-		gbc_listaEmpleados.fill = GridBagConstraints.VERTICAL;
-		gbc_listaEmpleados.gridx = 0;
-		gbc_listaEmpleados.gridy = 2;
-		panEmpleados.add(listaEmpleados, gbc_listaEmpleados);
-		
 		JPanel panModulos = new JPanel();
 		GridBagConstraints gbc_panModulos = new GridBagConstraints();
+		gbc_panModulos.insets = new Insets(0, 0, 0, 5);
 		gbc_panModulos.fill = GridBagConstraints.VERTICAL;
-		gbc_panModulos.gridx = 2;
+		gbc_panModulos.gridx = 1;
 		gbc_panModulos.gridy = 0;
 		frame.getContentPane().add(panModulos, gbc_panModulos);
 		GridBagLayout gbl_panModulos = new GridBagLayout();
@@ -160,13 +116,59 @@ public class Interfaz {
 		gbl_panModulos.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
 		panModulos.setLayout(gbl_panModulos);
 		
+		JLabel tituloModulos = new JLabel("Lista de modulos");
+		GridBagConstraints gbc_tituloModulos = new GridBagConstraints();
+		gbc_tituloModulos.insets = new Insets(0, 0, 5, 0);
+		gbc_tituloModulos.anchor = GridBagConstraints.NORTH;
+		gbc_tituloModulos.gridx = 0;
+		gbc_tituloModulos.gridy = 0;
+		panModulos.add(tituloModulos, gbc_tituloModulos);
+		
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_1.fill = GridBagConstraints.VERTICAL;
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 1;
+		panModulos.add(panel_1, gbc_panel_1);
+		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JButton button = new JButton("A\u00F1adir");
+		panel_1.add(button);
+		
+		JButton button_1 = new JButton("Eliminar");
+		panel_1.add(button_1);
+		
+		JPanel listaModulos = new JPanel();
+		GridBagConstraints gbc_listaModulos = new GridBagConstraints();
+		gbc_listaModulos.fill = GridBagConstraints.BOTH;
+		gbc_listaModulos.gridx = 0;
+		gbc_listaModulos.gridy = 2;
+		
+		
+		
+		panModulos.add(listaModulos, gbc_listaModulos);
+		
+		JPanel panEmnpleados = new JPanel();
+		GridBagConstraints gbc_panEmnpleados = new GridBagConstraints();
+		gbc_panEmnpleados.fill = GridBagConstraints.VERTICAL;
+		gbc_panEmnpleados.gridx = 2;
+		gbc_panEmnpleados.gridy = 0;
+		frame.getContentPane().add(panEmnpleados, gbc_panEmnpleados);
+		GridBagLayout gbl_panEmnpleados = new GridBagLayout();
+		gbl_panEmnpleados.columnWidths = new int[]{91, 0};
+		gbl_panEmnpleados.rowHeights = new int[]{14, 27, 0, 0};
+		gbl_panEmnpleados.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panEmnpleados.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		panEmnpleados.setLayout(gbl_panEmnpleados);
+		
 		JLabel tituloEmpleados = new JLabel("Lista de empleados");
 		GridBagConstraints gbc_tituloEmpleados = new GridBagConstraints();
 		gbc_tituloEmpleados.anchor = GridBagConstraints.NORTH;
 		gbc_tituloEmpleados.insets = new Insets(0, 0, 5, 0);
 		gbc_tituloEmpleados.gridx = 0;
 		gbc_tituloEmpleados.gridy = 0;
-		panModulos.add(tituloEmpleados, gbc_tituloEmpleados);
+		panEmnpleados.add(tituloEmpleados, gbc_tituloEmpleados);
 		
 		JPanel panel_3 = new JPanel();
 		GridBagConstraints gbc_panel_3 = new GridBagConstraints();
@@ -174,7 +176,7 @@ public class Interfaz {
 		gbc_panel_3.insets = new Insets(0, 0, 5, 0);
 		gbc_panel_3.gridx = 0;
 		gbc_panel_3.gridy = 1;
-		panModulos.add(panel_3, gbc_panel_3);
+		panEmnpleados.add(panel_3, gbc_panel_3);
 		panel_3.setLayout(new GridLayout(0, 2, 0, 0));
 		
 		JButton button_2 = new JButton("A\u00F1adir");
@@ -183,12 +185,12 @@ public class Interfaz {
 		JButton button_3 = new JButton("Eliminar");
 		panel_3.add(button_3);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-		gbc_scrollPane.fill = GridBagConstraints.VERTICAL;
-		gbc_scrollPane.gridx = 0;
-		gbc_scrollPane.gridy = 2;
-		panModulos.add(scrollPane, gbc_scrollPane);
+		JPanel listaEmpleados = new JPanel();
+		GridBagConstraints gbc_listaEmpleados = new GridBagConstraints();
+		gbc_listaEmpleados.fill = GridBagConstraints.BOTH;
+		gbc_listaEmpleados.gridx = 0;
+		gbc_listaEmpleados.gridy = 2;
+		panEmnpleados.add(listaEmpleados, gbc_listaEmpleados);
 	}
 
 }
