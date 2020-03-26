48546d0ed184f9c1bbe345ac509a4babd646a65e
AdrianRubioT <35344029+AdrianRubioT@users.noreply.github.com>
2020-03-26 18:22:44 +0100

    movido el la construccion de la interfaz a esta clase

---
+++ src/controlador/ControladorInterfaz.java
@@ -0,0 +1,22 @@
+package controlador;
+
+import vista.elemetos.ControlRadioButon;
+import vista.interfaz.Interfaz;
+
+/**
+ * clase para controlar la interfaz asi como susu eventos y comunicarlo al controlador princiapal
+ * @author Adrian
+ *
+ */
+public class ControladorInterfaz {
+
+	Interfaz interfaz = new Interfaz();
+	
+	
+	
+	public ControladorInterfaz() {
+
+	
+	}
+
+}
