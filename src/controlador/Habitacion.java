29ae4b6b4d2bb7470b0cbff80e1fe2d7969edc82
AdrianRubioT <adrianalumnosmx@gmail.com>
2020-05-12 18:42:34 +0200

    clase con los atributos y metodos segun el diagrama de objetos

---
+++ src/controlador/Habitacion.java
@@ -0,0 +1,50 @@
+package controlador;
+
+import javax.swing.JFrame;
+
+public class Habitacion {
+
+	private String habitacion;
+	
+	
+	/**
+	 * Genera objeto Habitacion sin valores
+	 */
+	public Habitacion() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	/**
+	 * Muestra una ventana para ingresar los datos que contendra el objeto
+	 * @param padre
+	 */
+	public Habitacion(JFrame padre) {
+		super();
+		
+		
+		
+	}
+
+
+
+
+
+	/**
+	 * @return the habitacion
+	 */
+	public String getHabitacion() {
+		return habitacion;
+	}
+	
+	/**
+	 * @param habitacion the habitacion to set
+	 */
+	public void setHabitacion(String habitacion) {
+		this.habitacion = habitacion;
+	}
+	
+	
+	
+	
+}
