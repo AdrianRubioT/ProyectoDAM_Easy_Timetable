cdd62481cda0fa26423fb60ee3e4b4445b2f6a5d
AdrianRubioT <adrianalumnosmx@gmail.com>
2020-05-13 19:51:49 +0200

    movido a la raiz del proyecto

--- src/main.java
+++ src/main.java
@@ -1,11 +1,14 @@
-package controlador;
 
+
+import controlador.controlador.Controlador;
 import vista.controlador.ControladorInterfaz;
 
 public class main {
 
 	public static void main(String[] args) {
 
+		new Controlador();
+		
 		new ControladorInterfaz();
 		
 		
