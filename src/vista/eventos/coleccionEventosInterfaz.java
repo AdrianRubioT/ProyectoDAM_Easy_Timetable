6b5c1f288bbdb2c9afdb9f9542c8fdb1a30158ea
AdrianRubioT <adrianalumnosmx@gmail.com>
2020-05-13 19:33:34 +0200

    primera version de la clase, falta probar funcionamiento

---
+++ src/vista/eventos/coleccionEventosInterfaz.java
@@ -0,0 +1,36 @@
+package vista.eventos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Clase para coleccionar los eventos y los triggers 
+ * @author adrian
+ *
+ */
+public class coleccionEventosInterfaz {
+
+	//Evento AniadirObjeto
+	//Evento cuando en la clase ListaTargetas se pulsa aniadir, esta clase manda la informacion
+	//insertada en el dialogo mediante el evento
+	/**
+	 * lista de las clases a que llamar cuando se lanza el evento
+	 */
+	private static List<AniadirListado> listenerAniadirObjeto = new ArrayList<AniadirListado>();
+	/**
+	 * aniade el parametro a l alista de listenerListado
+	 * @param toAdd clase que sera llamada cuando se lance el evento
+	 */
+	public static void addListener(AniadirListado toAdd) {
+		listenerAniadirObjeto.add(toAdd);
+	}
+	/**
+	 * trigger para el evento AniadirObjeto
+	 */
+	public static void triggerAniadirObjeto() {
+		for (AniadirListado hl : listenerAniadirObjeto) {
+			hl.onAddObject();
+		}
+	}
+
+}
