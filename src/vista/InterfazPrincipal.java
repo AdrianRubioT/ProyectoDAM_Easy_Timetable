c5ff30eda9e16a7e8c4533d972d46292d0977097
AdrianRubioT <adrianalumnosmx@gmail.com>
2020-05-26 19:31:32 +0200

    repasado y rellenado, muestra la interfaz y los botones

--- src/vista/InterfazPrincipal.java
+++ src/vista/InterfazPrincipal.java
@@ -1,22 +1,139 @@
-package PorColocar;
+package vista;
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import PorColocar.AreaTrabajo;
+import vista.elementos.ListaTarjetas;
+
 /**
  * Clase para montar la interfaz principal y su componentes
  * @author
  *
  */
 public class InterfazPrincipal {
-	private ListaTagetas listaHabitaciones = new ListaTargetas(Habitaciones);
-	private ListaTargetas listaAsiganturas;
-	private ListaTargetas listadocentes;
+	
+	private JFrame frame;
+	
+	
 	/**
 	 * listados para mostrar en la interfaz
 	 */
-	private ListaTargetas listaAlumnos;
-	private Areatrabajo areaTrabajo;
+	private ListaTarjetas listaHabitaciones;
+	private ListaTarjetas listaAsiganturas;
+	private ListaTarjetas listaDocentes;
+	private ListaTarjetas listaAlumnos;
+	
+	private AreaTrabajo areaTrabajo;
+	private ListaTarjetas tabAsignatura;
+	private ListaTarjetas tabDocente;
+	private ListaTarjetas tabHabitaciones;
+	private ListaTarjetas tabGrupoAlumnos;
 
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	public InterfazPrincipal() {
-		
+		initialize();
+		frame.setVisible(true);
+	}
+	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 645, 414);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 100, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+
+		JPanel panHorario = new JPanel();
+		GridBagConstraints gbc_panHorario = new GridBagConstraints();
+		gbc_panHorario.insets = new Insets(0, 0, 0, 5);
+		gbc_panHorario.fill = GridBagConstraints.BOTH;
+		gbc_panHorario.gridx = 0;
+		gbc_panHorario.gridy = 0;
+		frame.getContentPane().add(panHorario, gbc_panHorario);
+		GridBagLayout gbl_panHorario = new GridBagLayout();
+		gbl_panHorario.columnWidths = new int[]{0, 0};
+		gbl_panHorario.rowHeights = new int[]{0, 0, 0};
+		gbl_panHorario.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panHorario.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		panHorario.setLayout(gbl_panHorario);
+
+		JPanel panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.insets = new Insets(0, 0, 5, 0);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		panHorario.add(panel, gbc_panel);
+
+		JLabel lblHorario = new JLabel("Horario:");
+		panel.add(lblHorario);
+
+		JComboBox comboBox = new JComboBox();
+		panel.add(comboBox);
+
+		JButton btnAadir = new JButton("A\u00F1adir");
+		panel.add(btnAadir);
+
+		JButton btnEliminar = new JButton("Eliminar");
+		panel.add(btnEliminar);
+
+		JPanel areaTrabajo = new JPanel();
+		areaTrabajo.setBackground(Color.WHITE);
+		GridBagConstraints gbc_areaTrabajo = new GridBagConstraints();
+		gbc_areaTrabajo.fill = GridBagConstraints.BOTH;
+		gbc_areaTrabajo.gridx = 0;
+		gbc_areaTrabajo.gridy = 1;
+		panHorario.add(areaTrabajo, gbc_areaTrabajo);
+
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		GridBagConstraints gbc_tabbedPane = new GridBagConstraints();
+		gbc_tabbedPane.fill = GridBagConstraints.BOTH;
+		gbc_tabbedPane.gridx = 1;
+		gbc_tabbedPane.gridy = 0;
+		frame.getContentPane().add(tabbedPane, gbc_tabbedPane);
+
+
+		tabAsignatura = new ListaTarjetas("Asignaturas");
+		tabbedPane.addTab("Asignaturas", tabAsignatura);
+
+		tabDocente = new ListaTarjetas("Docente");
+		tabbedPane.addTab("Docente", tabDocente);
+
+		tabHabitaciones = new ListaTarjetas("Habitaciones");
+		tabbedPane.addTab("Habitaciones", tabHabitaciones);
+
+		tabGrupoAlumnos = new ListaTarjetas("GrupoAlumnos");
+		tabbedPane.addTab("GrupoAlumnos", tabGrupoAlumnos);
+
+
+
+		frame.setVisible(true);
 	}
 
+	
+	
+	
 }
 
--- src/vista/elementos/ListaTarjetas.java
+++ src/vista/elementos/ListaTarjetas.java
@@ -1,25 +1,86 @@
-package PorColocar;
+package vista.elementos;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 /**
  * Clase para listar los objetos Habitacion, Clase, docente y grupo alumno
  * @author
  *
  */
-public class ListaTarjetas {
+public class ListaTarjetas extends JPanel{
+	
 	public String titulo;
-	private Jbutton botonAniadir;
-	private Jbutton botonEliminar;
+	
+	
+	private JButton botonAniadir;
+	private JButton botonEliminar;
 	private JScrollPane listado;
 
-	public ListaTarjetas() {
-		
-	}
+	private JPanel lista;
+
 
 	/**
 	 * @param titulo
 	 * @return void
 	 */
-	public void ListaTarjetas(String titulo) {
-		// TODO Auto-generated method stub
+	public ListaTarjetas(String titulo) {
+		super();
+
+		this.titulo = titulo;
+
+
+		GridBagLayout gbl_panModulos = new GridBagLayout();
+		gbl_panModulos.columnWidths = new int[]{91, 0};
+		gbl_panModulos.rowHeights = new int[]{14, 27, 0, 0};
+		gbl_panModulos.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panModulos.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		setLayout(gbl_panModulos);
+
+		JLabel tituloModulos = new JLabel(titulo);
+		GridBagConstraints gbc_tituloModulos = new GridBagConstraints();
+		gbc_tituloModulos.insets = new Insets(0, 0, 5, 0);
+		gbc_tituloModulos.gridx = 0;
+		gbc_tituloModulos.gridy = 0;
+		add(tituloModulos, gbc_tituloModulos);
+
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_1.fill = GridBagConstraints.VERTICAL;
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 1;
+		add(panel_1, gbc_panel_1);
+		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+
+		botonAniadir = new JButton("A\u00F1adir");
+		panel_1.add(botonAniadir);
+
+		botonEliminar = new JButton("Eliminar");
+		panel_1.add(botonEliminar);
+
+		listado = new JScrollPane();
+		GridBagConstraints gbc_listado = new GridBagConstraints();
+		gbc_listado.fill = GridBagConstraints.BOTH;
+		gbc_listado.gridx = 0;
+		gbc_listado.gridy = 2;
+		add(listado, gbc_listado);
+
+		lista = new JPanel();
+
+		// con preferredSiza coloca los elemetos en vertical pero no muestra el scroll
+		lista.setPreferredSize(new Dimension(listado.getWidth(), 0));
+		//lista.setPreferredSize(listado.getSize());
+
+		listado.setViewportView(lista);
+
 	}
 
 	/**
