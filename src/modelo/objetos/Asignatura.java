4a838f24374d8ad48da6f67db4dc3f095cd16141
AdrianRubioT <adrianalumnosmx@gmail.com>
2020-05-27 20:34:05 +0200

    implementa la interfaz passInfoData y sus metodos, aniadido atributos staticos con los campos que tiene la clase

--- src/modelo/objetos/Asignatura.java
+++ src/modelo/objetos/Asignatura.java
@@ -1,13 +1,21 @@
 package modelo.objetos;
+
+import vista.elementos.ListaTargetas.PassInfoData;
+
 /**
  * Clase con las propiedades de una asignatura
  * @author
  *
  */
-public class Asignatura {
+public class Asignatura implements PassInfoData{
 	
 	private String nombre;
-
+	
+	/**
+	 * Lista de la clave de los atributos que tiene la clase
+	 */
+	public static String[]campos = {"nombre"}; 
+	
 	/**
 	 * @param nombre
 	 */
@@ -27,6 +35,29 @@ public class Asignatura {
 	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the campos
+	 */
+	public String[] getCampos() {
+		return campos;
+	}
+	
+	
+	/**
+	 * 
+	 */
+	@Override
+	public String[] getKeys() {
+		return campos;
+	}
+
+	@Override
+	public String[] getValues() {
+		// TODO Auto-generated method stub
+		String[] toReturn = {nombre};
+		return toReturn;
 	}
 
 	
