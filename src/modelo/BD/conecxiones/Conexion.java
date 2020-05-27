4428ed3b58800879d1e78e4ada13967a3e85a8cf
AdrianRubioT <adrianalumnosmx@gmail.com>
2020-05-27 12:56:46 +0200

    creado metodo para establecer la conexion SQLite

--- src/modelo/BD/conecxiones/Conexion.java
+++ src/modelo/BD/conecxiones/Conexion.java
@@ -1,15 +1,44 @@
 package modelo.BD.conecxiones;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * Clase para servir una conexion de una BD SQLite
  * @author
  *
  */
-public abstract class Conecxiones {
+public class Conexion {
 
-	public Conecxiones() {
+	/**
+	 * Establebe la conexion con una BD SQLite
+	 * Genera el archivo en path si este no existe
+	 * @param path BD SQLite donde conectar o crear la BD
+	 * @return conexion con la BD SQLite en path
+	 */
+	public static Connection conexion(String path) {
+		//preparamos la ruta de la BD
+		String url = "jdbc:sqlite:" + path;
+
+		//intentar conecctar a la BD creada
+		try (Connection conn = DriverManager.getConnection(url)) {
+			if (conn != null) {
+				DatabaseMetaData meta = conn.getMetaData();
+				
+				//test /chibatos
+				System.out.println("The driver name is " + meta.getDriverName());
+				System.out.println("A new database has been created.");
+				
+				return conn;
+				//conn.close();
+			}
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
 		
 	}
 
