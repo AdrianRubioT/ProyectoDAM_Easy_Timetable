b76e81a9b56ca7d3430ce2881fbbd14f42bf48c0
AdrianRubioT <adrianalumnosmx@gmail.com>
2020-05-27 12:58:33 +0200

    corregido los metodos con return Void, haciendo metodo para establecer un nuevo horario

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -1,7 +1,9 @@
 package Controlador;
 
+import java.io.File;
 import java.util.ArrayList;
 
+import javax.swing.JFileChooser;
 import modelo.ControaldorBD;
 import modelo.objetos.Asignatura;
 import modelo.objetos.Docente;
@@ -23,17 +25,17 @@ public class Controlador {
 	private ArrayList<Docente> listaDocente;
 	private ArrayList<GrupoAlumnos> listaGruposAlumnos;
 
-	//private ControaldorBD controladorBD = new ControaldorBD();
-	private InterfazPrincipal interfazPrincipal = new InterfazPrincipal();
-	
+	private ControaldorBD controladorBD = new ControaldorBD();
+	private InterfazPrincipal interfazPrincipal = new InterfazPrincipal(this);
+
 	public Controlador() {
-		
+
 	}
+
 
 	/**
 	 * Inicializa objeto habitacion y lo aniade a la coleccion de habitacion
 	 * @param datos 
-	 * @return void
 	 */
 	private void crearHabitacion(String[] datos) {
 		// TODO Auto-generated method stub
@@ -42,7 +44,6 @@ public class Controlador {
 	/**
 	 * Crea objeto Asignatura y lo lista en colAsignatura
 	 * @param datos
-	 * @return void
 	 */
 	private void crearAsignatura(String[] datos) {
 		// TODO Auto-generated method stub
@@ -51,7 +52,6 @@ public class Controlador {
 	/**
 	 * Crea objeto docente y lo lista en colDocente
 	 * @param datos
-	 * @return void
 	 */
 	private void crearDocente(String[] datos) {
 		// TODO Auto-generated method stub
@@ -63,6 +63,33 @@ public class Controlador {
 	 */
 	private void crearGrupoAlumno(String[] datos) {
 		// TODO Auto-generated method stub
+	}
+
+
+	/**
+	 * evento para indicar un nuevo archivo del horario
+	 */
+	public void NuevoHorario() {
+
+		JFileChooser fileChooser = new JFileChooser("./");
+
+		//mostrar el dialogo para seleccionar la carpeta
+		int seleccion = fileChooser.showSaveDialog( interfazPrincipal );
+
+		//comprovacion que el usuario ha pulsado el boton guardar
+		if (seleccion == JFileChooser.APPROVE_OPTION)
+		{
+
+			//Obtenemos el fichero seleccionado 
+			File fichero = fileChooser.getSelectedFile();
+			controladorBD.conecta( fichero.getPath(), fichero.exists() );
+			//System.out.println(fichero.getPath() );
+			
+			//System.out.println("El fichero existe: " +  fichero.exists() );
+			
+		}
+
+
 	}
 
 }
