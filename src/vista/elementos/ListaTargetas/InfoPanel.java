0cb14e9c99d103e28f532fc8f1e6d7edec8043d6
AdrianRubioT <adrianalumnosmx@gmail.com>
2020-05-28 18:03:51 +0200

    quitado atributo JPanel que duplicaba al exend JPanel, aniadido metodos para conseguir recoger un click en la misma instancia

--- src/vista/elementos/ListaTargetas/InfoPanel.java
+++ src/vista/elementos/ListaTargetas/InfoPanel.java
@@ -10,12 +10,18 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 /**
  * Pequeño JPanel con JLabels indicando las claves y sus valores
  * @author
  *
  */
-public class InfoPanel extends JPanel{
+public class InfoPanel extends JPanel {
 	/**
 	 * coleccion de JLabels generadas
 	 */
@@ -24,89 +30,97 @@ public class InfoPanel extends JPanel{
 	 * coleccion de los valores para las claves
 	 */
 	private ArrayList<JLabel> valores;
-	private JPanel panDescripcion;
-
-	/**
-	 * @wbp.parser.entryPoint
-	 */
-	public InfoPanel(PassInfoData objeto) {
-		setBorder(new LineBorder(new Color(0, 0, 0), 2));
 
 
 
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{81, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
-		setLayout(gridBagLayout);
+		public InfoPanel(PassInfoData objeto) {
+			//TODO: quitar mas adelante es muy feo
+			setBorder(new LineBorder(new Color(0, 0, 0), 2));
 
 
-		panDescripcion = new JPanel();
-		GridBagConstraints gbc_panDescripcion = new GridBagConstraints();
-		gbc_panDescripcion.fill = GridBagConstraints.BOTH;
-		gbc_panDescripcion.gridx = 1;
-		gbc_panDescripcion.gridy = 0;
-		add(panDescripcion, gbc_panDescripcion);
-		GridBagLayout gbl_panDescripcion = new GridBagLayout();
-		gbl_panDescripcion.columnWidths = new int[]{0};
-		gbl_panDescripcion.rowHeights = new int[]{0};
-		gbl_panDescripcion.columnWeights = new double[]{Double.MIN_VALUE};
-		gbl_panDescripcion.rowWeights = new double[]{Double.MIN_VALUE};
-		panDescripcion.setLayout(gbl_panDescripcion);
-		
-		
-		addInfoObject(objeto);
-		
-		
-	}
 
-	/**
-	 * extrae del parametro objeto la informacion para crear los pares de JLabels
-	 * clave-valor
-	 * @param objeto
-	 * @return void
-	 */
-	public void addInfoObject(PassInfoData objeto) {
-		
-		String[] etiquetas = objeto.getKeys();
-		String[] claves = objeto.getValues();
-		
-		//etiquetas
-		for (int i = 0; i < etiquetas.length; i++) {
-			//System.out.println(etiquetas[i]);
-			JLabel JPKey = new JLabel( etiquetas[i]);
-			GridBagConstraints gbc_lblLinea_3 = new GridBagConstraints();
-			gbc_lblLinea_3.insets = new Insets(0, 0, 0, 5);
-			gbc_lblLinea_3.gridx = 0;
-			gbc_lblLinea_3.gridy = i;
-			panDescripcion.add(JPKey, gbc_lblLinea_3);
-		}
+			GridBagLayout gridBagLayout = new GridBagLayout();
+			gridBagLayout.columnWidths = new int[]{81, 0, 0};
+			gridBagLayout.rowHeights = new int[]{0, 0};
+			gridBagLayout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+			gridBagLayout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+			setLayout(gridBagLayout);
 
-		
-		for (int i = 0; i < claves.length; i++) {
-			//System.out.println(data[i]);
-			JLabel JPValue = new JLabel( claves[i]);
-			GridBagConstraints gbc_lin4 = new GridBagConstraints();
-			gbc_lin4.fill = GridBagConstraints.HORIZONTAL;
-			gbc_lin4.gridx = 1;
-			gbc_lin4.gridy = i;
-			panDescripcion.add( JPValue, gbc_lin4);
+
+			//panDescripcion = new JPanel();
+
+
+//			GridBagConstraints gbc_panDescripcion = new GridBagConstraints();
+//			gbc_panDescripcion.fill = GridBagConstraints.BOTH;
+//			gbc_panDescripcion.gridx = 1;
+//			gbc_panDescripcion.gridy = 0;
+//			add(panDescripcion, gbc_panDescripcion);
+			GridBagLayout gbl_panDescripcion = new GridBagLayout();
+			gbl_panDescripcion.columnWidths = new int[]{0};
+			gbl_panDescripcion.rowHeights = new int[]{0};
+			gbl_panDescripcion.columnWeights = new double[]{Double.MIN_VALUE};
+			gbl_panDescripcion.rowWeights = new double[]{Double.MIN_VALUE};
+			setLayout(gbl_panDescripcion);
+
+
+			addInfoObject(objeto);
+
 
 		}
 
-	}
+		/**
+		 * extrae del parametro objeto la informacion para crear los pares de JLabels
+		 * clave-valor
+		 * @param objeto
+		 * @return void
+		 */
+		public void addInfoObject(PassInfoData objeto) {
 
-	/**
-	 * Crea tantos JTextfield como inputs hayan y lo agrega a colInputs ademas d
-	 * e agregarlos a la intefaz
-	 * @param etiquetas
-	 * @param data
-	 * @return void
-	 */
-	private void crearLabels(String[] etiquetas, String[] data) {
-		// TODO Auto-generated method stub
-	}
+			String[] etiquetas = objeto.getKeys();
+			String[] claves = objeto.getValues();
 
+			//etiquetas
+			for (int i = 0; i < etiquetas.length; i++) {
+				//System.out.println(etiquetas[i]);
+				JLabel JPKey = new JLabel( etiquetas[i]);
+				GridBagConstraints gbc_lblLinea_3 = new GridBagConstraints();
+				gbc_lblLinea_3.insets = new Insets(0, 0, 0, 5);
+				gbc_lblLinea_3.gridx = 0;
+				gbc_lblLinea_3.gridy = i;
+				add(JPKey, gbc_lblLinea_3);
+			}
+
+
+			for (int i = 0; i < claves.length; i++) {
+				//System.out.println(data[i]);
+				JLabel JPValue = new JLabel( claves[i]);
+				GridBagConstraints gbc_lin4 = new GridBagConstraints();
+				gbc_lin4.fill = GridBagConstraints.HORIZONTAL;
+				gbc_lin4.gridx = 1;
+				gbc_lin4.gridy = i;
+				add( JPValue, gbc_lin4);
+
+			}
+
+		}
+
+		public void chivato(MouseEvent e) {
+			InfoPanel a = (InfoPanel) e.getSource();
+
+			System.out.println( a.getValores().get(0).getText() );
+		}
+
+		/**
+		 * @return the valores
+		 */
+		public ArrayList<JLabel> getValores() {
+			return valores;
+		}
+
+
+
+
+
+	
 }
 
