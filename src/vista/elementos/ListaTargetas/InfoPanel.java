fd66e8384ba4e4a9cefae3b2a74b230900e887ae
AdrianRubioT <adrianalumnosmx@gmail.com>
2020-05-28 12:28:22 +0200

    aniadido la estructura visual del panel

--- src/vista/elementos/ListaTargetas/InfoPanel.java
+++ src/vista/elementos/ListaTargetas/InfoPanel.java
@@ -1,15 +1,21 @@
 package vista.elementos.ListaTargetas;
 
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.util.ArrayList;
 
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 /**
  * Pequeño JPanel con JLabels indicando las claves y sus valores
  * @author
  *
  */
-public class InfoPanel {
+public class InfoPanel extends JPanel{
 	/**
 	 * coleccion de JLabels generadas
 	 */
@@ -18,8 +24,40 @@ public class InfoPanel {
 	 * coleccion de los valores para las claves
 	 */
 	private ArrayList<JLabel> valores;
+	private JPanel panDescripcion;
 
-	public InfoPanel() {
+	/**
+	 * @wbp.parser.entryPoint
+	 */
+	public InfoPanel(PassInfoData objeto) {
+		setBorder(new LineBorder(new Color(0, 0, 0), 2));
+
+
+
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{81, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+
+
+		panDescripcion = new JPanel();
+		GridBagConstraints gbc_panDescripcion = new GridBagConstraints();
+		gbc_panDescripcion.fill = GridBagConstraints.BOTH;
+		gbc_panDescripcion.gridx = 1;
+		gbc_panDescripcion.gridy = 0;
+		add(panDescripcion, gbc_panDescripcion);
+		GridBagLayout gbl_panDescripcion = new GridBagLayout();
+		gbl_panDescripcion.columnWidths = new int[]{0};
+		gbl_panDescripcion.rowHeights = new int[]{0};
+		gbl_panDescripcion.columnWeights = new double[]{Double.MIN_VALUE};
+		gbl_panDescripcion.rowWeights = new double[]{Double.MIN_VALUE};
+		panDescripcion.setLayout(gbl_panDescripcion);
+		
+		
+		addInfoObject(objeto);
+		
 		
 	}
 
@@ -30,7 +68,33 @@ public class InfoPanel {
 	 * @return void
 	 */
 	public void addInfoObject(PassInfoData objeto) {
-		// TODO Auto-generated method stub
+		
+		String[] etiquetas = objeto.getKeys();
+		String[] claves = objeto.getValues();
+		
+		//etiquetas
+		for (int i = 0; i < etiquetas.length; i++) {
+			//System.out.println(etiquetas[i]);
+			JLabel JPKey = new JLabel( etiquetas[i]);
+			GridBagConstraints gbc_lblLinea_3 = new GridBagConstraints();
+			gbc_lblLinea_3.insets = new Insets(0, 0, 0, 5);
+			gbc_lblLinea_3.gridx = 0;
+			gbc_lblLinea_3.gridy = i;
+			panDescripcion.add(JPKey, gbc_lblLinea_3);
+		}
+
+		
+		for (int i = 0; i < claves.length; i++) {
+			//System.out.println(data[i]);
+			JLabel JPValue = new JLabel( claves[i]);
+			GridBagConstraints gbc_lin4 = new GridBagConstraints();
+			gbc_lin4.fill = GridBagConstraints.HORIZONTAL;
+			gbc_lin4.gridx = 1;
+			gbc_lin4.gridy = i;
+			panDescripcion.add( JPValue, gbc_lin4);
+
+		}
+
 	}
 
 	/**
