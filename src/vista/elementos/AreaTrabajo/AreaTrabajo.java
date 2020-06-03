1fe8dd6200ad7289ff6511aa5984ba3432f3644d
AdrianRubioT <adrianalumnosmx@gmail.com>
2020-06-03 17:05:33 +0200

    inicio desarrollo del area de trabajo

---
+++ src/vista/elementos/AreaTrabajo/AreaTrabajo.java
@@ -0,0 +1,14 @@
+package vista.elementos.AreaTrabajo;
+
+import javax.swing.JPanel;
+
+public class AreaTrabajo extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public AreaTrabajo() {
+
+	}
+
+}
